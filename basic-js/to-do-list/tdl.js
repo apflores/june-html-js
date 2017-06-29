d02065a07a4577f7629d7d05071c5f8252e84f98
apflores <dear.floresa@gmail.com>
2017-06-29 16:56:30 -0400

    to-do list completed commit

--- basic-js/to-do-list/tdl.js
+++ basic-js/to-do-list/tdl.js
@@ -2,24 +2,36 @@
 
 var c = console;
 
-window.onload = function() {
+window.onload = function () {
     var add = document.getElementById("add");
     var task = document.getElementById("task");
     var list = document.getElementById("list");
-    var li = document.createElement("li");
-    
-    add.addEventListener("click", function() {
-        li.appendChild(task.value);
-        //list.innerHTML += ("<li>" + task.value + "</li>");
+    var finished = document.getElementById("finished");
+
+    add.addEventListener("click", function () {
+        var li = document.createElement("li");
+        li.innerHTML = (task.value + "  <button value='check'>✓</button> <button value='x'>X</button>");
+        list.appendChild(li);
         task.value = "";
     });
 
-    list.addEventListener("click", function() {
-        list.innerHTML = "";
+    list.addEventListener("click", function (event) {
+        var item = event.target;
+        if (item.value == "x") {
+            list.removeChild(item.parentElement);
+        } else if(item.value == "check") {
+            finished.appendChild(item.parentElement);
+            c.log(list);
+            c.log(finished);
+        }
     });
 
-    function todo(id, task) {
-        this.i = id;
-        this.t = task;
-    }
+    finished.addEventListener("click", function(event) {
+        var item = event.target;
+        if (item.value == "x") {
+            finished.removeChild(item.parentElement);
+        } else if(item.value == "check") {
+            list.appendChild(item.parentElement);
+        }
+    });
 }
